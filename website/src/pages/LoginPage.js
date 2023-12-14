a4c83fd36deeb7924e5dbaa4f181c871a4e47baa
Briana Torres <briana@torrez.us>
2023-12-14 00:58:14 -0500

    fixing login screen

--- website/src/pages/LoginPage.js
+++ website/src/pages/LoginPage.js
@@ -8,6 +8,8 @@ function LoginPage() {
   const [isLogin, setIsLogin] = useState(true);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
   const [message, setMessage] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   let history = useHistory();
@@ -21,6 +23,10 @@ function LoginPage() {
       setMessage('Username and password must be less than 20 characters.');
       return false;
     }
+    if (!isLogin && (email.length > 40 || name.length > 40)) {
+      setMessage('Email and name must be less than 40 characters.');
+      return false;
+    }
     return true;
   };
 
@@ -32,25 +38,27 @@ function LoginPage() {
         localStorage.setItem('currentUser', username);
         setMessage('Logged in successfully!');
         history.push('/home');
-      } else if (!users[username]) {
-        setMessage('The username does not exist.');
-      } else if (users[username] && users[username].password !== password) {
-        setMessage('The password is incorrect.');
       } else {
         setMessage('Invalid credentials. Please try again.');
       }
     }
-  };  
+  };
 
   const handleSignUp = (event) => {
     event.preventDefault();
     if (validateForm()) {
       const users = JSON.parse(localStorage.getItem('users')) || {};
       if (!users[username]) {
-        // Create a new user object
         const newUser = {
           password: password,
-          pastSessions: []  // Initialize an empty array for past study sessions
+          email: email,
+          name: name,
+          groups: [],
+          sessions: [],
+          major: null,
+          graduationYear: null,
+          ethnicity: null,
+          gender: null
         };
         users[username] = newUser;
         localStorage.setItem('users', JSON.stringify(users));
@@ -62,7 +70,6 @@ function LoginPage() {
       }
     }
   };
-  
 
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
@@ -72,81 +79,103 @@ function LoginPage() {
     event.preventDefault();
   };
 
-    return (
-        <Container maxWidth={false} disableGutters sx={{mb: 0}}>
-        <Box sx={{ mt: -10, pt: 0, display: 'flex', flexDirection: 'column', alignItems: 'center', 
-        width: '100vw', height: '100vh', justifyContent: 'center' }}>
-             <Typography component="h1" variant="h1" sx={{ fontSize: '5rem',
-              fontWeight: 'bold', textAlign: 'center', width: '100%', mb: 8, mt: 0}}>
-            The Study Community
-            </Typography>
-            <Typography component="h1" variant="h5">
-              {isLogin ? 'Login' : 'Sign Up'}
-            </Typography>
-            <Box component="form" onSubmit={isLogin ? handleLogin : handleSignUp} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                label="Username"
-                name="username"
-                autoComplete="username"
-                autoFocus
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                inputProps={{ maxLength: 20 }}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type={showPassword ? 'text' : 'password'}
-                autoComplete="current-password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                inputProps={{ maxLength: 20 }}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                {isLogin ? 'Login' : 'Sign Up'}
-              </Button>
-              <Stack direction="row" spacing={2} justifyContent="center">
-                <Button
-                  variant="text"
-                  onClick={() => setIsLogin(!isLogin)}
+  return (
+    <Container sx={{ width: '98%', display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', justifyContent: 'center' }}>
+      <Typography component="h1" variant="h1" gutterBottom sx={{textAlign: 'center', fontWeight: 'bold', fontSize: '5rem'}}>
+        The Study Community
+      </Typography>
+      <Typography component="h2" variant="h5" sx={{ mb: 4 }}>
+        {isLogin ? 'Login' : 'Sign Up'}
+      </Typography>
+      <Box component="form" onSubmit={isLogin ? handleLogin : handleSignUp} sx={{ width: '50%', mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          label="Username"
+          name="username"
+          autoComplete="username"
+          autoFocus
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          inputProps={{ maxLength: 20 }}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type={showPassword ? 'text' : 'password'}
+          autoComplete="current-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          inputProps={{ maxLength: 20 }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
                 >
-                  {isLogin ? 'Need an account? Sign Up' : 'Already have an account? Login'}
-                </Button>
-              </Stack>
-              {message && (
-                <Typography color="secondary" variant="body2" align="center" sx={{ mt: 2 }}>
-                  {message}
-                </Typography>
-              )}
-            </Box>
-          </Box>
-        </Container>
-      );
-    }
-    
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        {!isLogin && (
+          <>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Email"
+              name="email"
+              autoComplete="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              inputProps={{ maxLength: 40 }}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Name"
+              name="name"
+              autoComplete="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              inputProps={{ maxLength: 40 }}
+            />
+          </>
+        )}
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          {isLogin ? 'Login' : 'Sign Up'}
+        </Button>
+        <Stack direction="row" spacing={2} justifyContent="center">
+          <Button
+            variant="text"
+            onClick={() => setIsLogin(!isLogin)}
+          >
+            {isLogin ? 'Need an account? Sign Up' : 'Already have an account? Login'}
+          </Button>
+        </Stack>
+        {message && (
+          <Typography color="secondary" variant="body2" align="center" sx={{ mt: 2 }}>
+            {message}
+          </Typography>
+        )}
+      </Box>
+    </Container>
+  );
+}
+
 export default LoginPage;
--- website/src/pages/ProfilePage.js
+++ website/src/pages/ProfilePage.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import { Box, Typography, TextField, IconButton, Container, AppBar, Toolbar } from '@mui/material';
+import React,  { useState } from 'react';
+import { Box, Typography, TextField, IconButton, Container, InputAdornment , AppBar, Toolbar } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useHistory } from 'react-router-dom';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const ProfilePage = () => {
   let history = useHistory(); 
+  const [showPassword, setShowPassword] = useState(false);
   
   const handleBack = () => {
     history.goBack();
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
   };
 
   return (
     <>
       <AppBar position="static" color="transparent" elevation={0} sx={{mt: 2}}>
         <Toolbar>
-          {/* <IconButton edge="start" color="inherit" aria-label="home" onClick={handleHomeClick}>
-            <HomeIcon />
-          </IconButton> */}
           <IconButton onClick={handleBack} sx={{ visibility: 'visible' }}>
           <ArrowBackIcon fontSize="large" />
-        </IconButton>
+          </IconButton>
           <Typography variant="h4" component="h3" style={{ flexGrow: 1, textAlign: 'center' }}>
             Account Information
           </Typography>
@@ -28,11 +36,30 @@ const ProfilePage = () => {
       <Container maxWidth="sm" sx={{mt: 4}}>
         {/* The rest of your form */}
         <Box component="form" sx={{'& > :not(style)': { m: 1 } }}>
-          <TextField label="Name" variant="outlined" fullWidth />
-          <TextField label="Email" variant="outlined" fullWidth />
-          <TextField label="Username" variant="outlined" fullWidth />
-          <TextField label="Ethnic Background" variant="outlined" fullWidth />
-          <TextField label="Year" variant="outlined" fullWidth />
+          <TextField label="Name" required variant="outlined" fullWidth />
+          <TextField label="Email" required variant="outlined" fullWidth />
+          <TextField label="Username" required variant="outlined" fullWidth />
+          <TextField label="Password" 
+          inputProps={{ maxLength: 20 }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          required variant="outlined" fullWidth />
+          <TextField label="Major" variant="outlined" fullWidth />
+          <TextField label="Graduation Year" variant="outlined" fullWidth />
+          <TextField label="Ethnicity" variant="outlined" fullWidth />
+          <TextField label="Gender" variant="outlined" fullWidth />
           {/* ... add more fields as needed */}
         </Box>
       </Container>
